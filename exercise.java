6d61bf5d275f9e18541ad7c7be78ebd0be1288b6
Duarte Silva <dmcsilva@criticalsoftware.com>
2022-05-16 10:52:35 +0100

    altered the constructor

--- exercise.java
+++ exercise.java
@@ -1,7 +1,21 @@
 
 public class Transport{
 
+    int numberOfWheels;
+    boolean isItAutonomous;
+    int numberOfKm;
+
+
+    public Transport(int wheels, boolean autonomous, int kilometers) {
+         numberOfWheels = wheels;
+         isItAutonomous = autonomous;
+         numberOfKm = kilometers;
+    } 
+
+    
     public static void main(String[] args){
         System.out.println("Hello!!");
     }
+
+    
 }
